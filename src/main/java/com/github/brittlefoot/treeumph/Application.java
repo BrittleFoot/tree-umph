c473048e871c1fb72ca88c55b15c7d6c5a91811a
Ostanin Igor <ostanin.igor@skbkontur.ru>
2018-05-20 17:28:15 +0500

    config pulled down

--- src/main/java/com/github/brittlefoot/treeumph/Application.java
+++ src/main/java/com/github/brittlefoot/treeumph/Application.java
@@ -3,20 +3,12 @@ package com.github.brittlefoot.treeumph;
 
 import com.github.brittlefoot.treeumph.configuration.ApplicationConfiguration;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 
-@Configuration
-@Import({ApplicationConfiguration.class})
-@ComponentScan({"com.github.brittlefoot.treeumph.configuration"})
-@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ApplicationConfiguration.class, args);
     }
 
 }
--- src/main/java/com/github/brittlefoot/treeumph/configuration/ApplicationConfiguration.java
+++ src/main/java/com/github/brittlefoot/treeumph/configuration/ApplicationConfiguration.java
@@ -1,19 +1,22 @@
 package com.github.brittlefoot.treeumph.configuration;
 
 import com.github.brittlefoot.treeumph.persistence.MongoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-
 @Configuration
 @ComponentScan({
+        "com.github.brittlefoot.treeumph.configuration",
         "com.github.brittlefoot.treeumph.controller",
         "com.github.brittlefoot.treeumph.services"})
 @Import({MongoConfiguration.class})
+@EnableAutoConfiguration
 @EnableTransactionManagement
 public class ApplicationConfiguration {
 
 }
+
 
